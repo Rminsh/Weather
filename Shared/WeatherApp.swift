3dc9bc12fd08cc79a8132a9bd5f32626ade8052b
Armin <armin536@gmail.com>
2022-07-06 17:01:05 +0430

    Initial commit

---
+++ Shared/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Shared
+//
+//  Created by Armin on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
