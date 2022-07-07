8d5e55f2fe282edb94c414e989e7366db52a0885
Armin <armin536@gmail.com>
2022-07-08 01:23:50 +0430

    Added AddCityView

---
+++ Shared/Views/AddCityView.swift
@@ -0,0 +1,95 @@
+//
+//  AddCityView.swift
+//  Weather
+//
+//  Created by Armin on 7/8/22.
+//
+
+import SwiftUI
+
+struct AddCityView: View {
+    
+    @Binding var loading: Bool
+    @Binding var warningMessage: String
+    @State var action: () -> Void
+    
+    @State private var searchCity = ""
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        #if os(iOS)
+        NavigationView {
+            content
+                .navigationTitle("Add City")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: { presentationMode.wrappedValue.dismiss() }) {
+                            Text("Close")
+                        }
+                    }
+                }
+        }
+        #else
+        content
+            .frame(width: 320, height: 350)
+        #endif
+    }
+    
+    var content: some View {
+        VStack(spacing: 15) {
+            Text("🏙")
+                .font(.system(size: 100))
+                .dynamicTypeSize(.xSmall ... .xxLarge)
+                .lineLimit(1)
+                .minimumScaleFactor(0.4)
+                .padding()
+                .opacity(0.75)
+            
+            Text("Add City")
+            
+            Label {
+                TextField("City name", text: $searchCity)
+                    .frame(maxWidth: 250)
+                    
+            } icon: {
+                Image(systemName: "magnifyingglass")
+            }
+            .padding()
+            
+            if loading {
+                ProgressView()
+                    .progressViewStyle(.circular)
+            } else {
+                Button(action: action) {
+                    Text("Search & Add")
+                        .fontWeight(.semibold)
+                }
+                #if os(iOS)
+                .buttonStyle(.borderedProminent)
+                #elseif os(macOS)
+                .buttonStyle(.bordered)
+                #endif
+                .controlSize(.regular)
+                .headerProminence(.increased)
+            }
+            
+            Text(warningMessage)
+                .foregroundColor(.red)
+                .dynamicTypeSize(.xSmall ... .xxLarge)
+                .lineLimit(1)
+                .minimumScaleFactor(0.4)
+        }
+    }
+}
+
+struct AddCityView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCityView(
+            loading: .constant(false),
+            warningMessage: .constant(""),
+            action: {}
+        )
+    }
+}
