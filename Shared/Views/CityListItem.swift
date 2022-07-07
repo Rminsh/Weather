03e5a74af10090a5d2100b62b78add25fab7c5c3
Armin <armin536@gmail.com>
2022-07-07 18:24:44 +0430

    Cleanup CityDetail mock

--- Shared/Views/CityListItem.swift
+++ Shared/Views/CityListItem.swift
@@ -74,43 +74,7 @@ struct CityListItem: View {
 struct CityListItem_Previews: PreviewProvider {
     static var previews: some View {
         CityListItem(
-            city: CityDetail(
-                coord: Coord(
-                    lon: 11.9668,
-                    lat: 57.7072
-                ),
-                sys: Sys(
-                    country: "SE",
-                    timezone: 7200,
-                    sunrise: 1657074106,
-                    sunset: 1657138294
-                ),
-                weather: [
-                    WeatherDetail(
-                        id: 800,
-                        main: "Clear",
-                        weatherDescription: "clear sky",
-                        icon: "01d"
-                    )
-                ],
-                main: WeatherMain(
-                    temp: 18.04,
-                    feelsLike: 17.47,
-                    tempMin: 16.73,
-                    tempMax: 18.71,
-                    pressure: 1014,
-                    humidity: 60
-                ),
-                visibility: 10000,
-                wind: Wind(
-                    speed: 6.69,
-                    deg: 280
-                ),
-                clouds: Clouds(all: 1),
-                dt: 1657123756,
-                id: 2711537,
-                name: "Gothenburg"
-            )
+            city: CityDetail.mock
         )
         .padding()
     }
