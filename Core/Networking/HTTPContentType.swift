cfab4adf85afee9b16fcff50ad7452fe88c71a94
Armin <armin536@gmail.com>
2022-07-26 23:05:40 +0430

    Removed redundant HTTPContentType

--- Core/Networking/HTTPContentType.swift
+++ Core/Networking/HTTPContentType.swift
@@ -10,6 +10,5 @@ import Foundation
 public struct HTTPContentType {
     public static let charsetUTF8 = "charset=utf-8"
     public static let applicationJSON = "application/json"
-    public static let multipartFormData = "multipart/form-data"
 }
 
