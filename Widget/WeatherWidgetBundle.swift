1d22f008dd3984315288fe5a80de871fc088aaf0
Armin <armin536@gmail.com>
2023-08-22 09:10:46 +0330

    Added widget target

---
+++ Widget/WeatherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherWidgetBundle.swift
+//  WeatherWidget
+//
+//  Created by Armin on 8/22/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeatherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeatherWidget()
+    }
+}
