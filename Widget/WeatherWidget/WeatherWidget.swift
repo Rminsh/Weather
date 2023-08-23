ecf377ea4ffef1e1da373608dc75e6d44d4b9889
Armin <armin536@gmail.com>
2023-08-23 10:12:52 +0330

    Added basic implementation of widget

--- Widget/WeatherWidget/WeatherWidget.swift
+++ Widget/WeatherWidget/WeatherWidget.swift
@@ -9,49 +9,80 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+    
+    func placeholder(in context: Context) -> WeatherEntry {
+        WeatherEntry(date: .now,city: .mock)
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+    func getSnapshot(in context: Context, completion: @escaping (WeatherEntry) -> Void) {
+        let entry = WeatherEntry(date: .now,city: .mock)
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
+        Task {
+            let now = Date()
+            let nextUpdate = now.addingTimeInterval(1800) /// 30 minutes
+            let service = WeatherService()
+            let result = try await service.getWeatherOfCity(city: "Tehran", unit: "metric")
+            let entry = WeatherEntry(date: .now, city: result)
+            let timeline = Timeline(entries: [entry], policy: .after(nextUpdate))
+            completion(timeline)
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct WeatherEntry: TimelineEntry {
     let date: Date
-    let emoji: String
+    let city: WeatherOfCity
 }
 
 struct WeatherWidgetEntryView : View {
     var entry: Provider.Entry
 
+    var mf: MeasurementFormatter {
+        let mf = MeasurementFormatter()
+        mf.numberFormatter.maximumFractionDigits = 0
+        mf.unitOptions = .providedUnit
+        return mf
+    }
+    
+    var temp: String {
+        entry.city.main.cleanTemp.formatted(
+            .measurement(
+                width: .narrow,
+                usage: .weather,
+                numberFormatStyle: .number.precision(.fractionLength(0))
+            )
+        )
+    }
+    
     var body: some View {
-        VStack {
-            HStack {
-                Text("Time:")
-                Text(entry.date, style: .time)
+        HStack {
+            Text("\(entry.city.main.temp.formatted(.number.precision(.fractionLength(0))))°")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .fontDesign(.rounded)
+            
+            VStack {
+                Text(entry.city.name)
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.trailing)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Text(entry.city.weather.first?.main ?? "")
+                    .font(.caption)
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.trailing)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
             }
-
-            Text("Emoji:")
-            Text(entry.emoji)
         }
+        .foregroundStyle(.white)
+        .shadow(radius: 4)
+        .frame(maxHeight: .infinity, alignment: .bottom)
     }
 }
 
@@ -62,11 +93,19 @@ struct WeatherWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(macOS 14.0, iOS 17.0, *) {
                 WeatherWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+                    .containerBackground(for: .widget) {
+                        Image(entry.city.weather.first?.icon ?? "01d")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    }
             } else {
                 WeatherWidgetEntryView(entry: entry)
                     .padding()
-                    .background()
+                    .background {
+                        Image(entry.city.weather.first?.icon ?? "01d")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    }
             }
         }
         .configurationDisplayName("My Widget")
@@ -77,6 +116,5 @@ struct WeatherWidget: Widget {
 #Preview(as: .systemSmall) {
     WeatherWidget()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    WeatherEntry(date: .now, city: .mock)
 }
